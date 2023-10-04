ea10f954eea60bebd3d099e1e94bc7d6125c6160
flverge_42 <verge.f@gmail.com>
2023-10-04 10:30:43 +0200

    project start

--- main.c
+++ main.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 10:25:13 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/04 10:29:01 by flverge          ###   ########.fr       */
+/*   Created: 2023/10/04 10:22:36 by flverge           #+#    #+#             */
+/*   Updated: 2023/10/04 10:23:20 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# include <unistd.h> // read
-# include <stdlib.h> // malloc + free
-
-char	*get_next_line(int fd);
-
-
-
-#endif
+int main(void)
+{
+	
+}
