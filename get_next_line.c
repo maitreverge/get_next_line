8ee901e79cf908fcabcec15f880721af701481ed
flverge_42 <verge.f@gmail.com>
2023-10-07 14:39:38 +0200

    saving

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:24:42 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/07 14:28:17 by flverge          ###   ########.fr       */
+/*   Updated: 2023/10/07 14:33:38 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,30 @@ char	*get_next_line(int fd)
 {
 	int return_read;
 	static char *stash;
-	char	*buffer;
+	char	*original_buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	// Init du malloc du buffer lors de la compilation
-	buffer = (char*)ft_calloc(BUFFER_SIZE, 1);
-	if (!buffer)
+	original_buffer = (char*)ft_calloc(BUFFER_SIZE, 1);
+	if (!original_buffer)
 		return (NULL);
 	// faire boucler read
-	return_read = read(fd, buffer, BUFFER_SIZE);
+	return_read = read(fd, original_buffer, BUFFER_SIZE);
 	if (return_read < 0)
 		return (NULL);
 	else
 	{
-		reading_fd(fd, return_read, buffer)
+		reading_fd(fd, return_read, original_buffer)
 	}
 	return (stash);
 }
 
-void	reading_fd(int fd, int return_read, char *buffer)
+void	reading_fd(int fd, int return_read, char *original_buffer)
 {
+	char buff_size1[1];
 	if (return_read == 0)
 	{
-		
+		return (ft_strjoin(original_buffer, buff_size1));
 	}
 }
