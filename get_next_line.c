14598acee882ecf9cec3eb094fe410cb93b9501b
flverge_42 <verge.f@gmail.com>
2023-10-08 15:41:31 +0200

    save work

--- get_next_line.c
+++ get_next_line.c
@@ -6,61 +6,38 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:24:42 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/08 11:47:54 by flverge          ###   ########.fr       */
+/*   Updated: 2023/10/08 15:28:36 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int check_endoline(char *original_buffer)
+char 	*reset_stash(int fd, char *stash)
 {
-	int i;
-
-	i = 0;
-	while (original_buffer[i])
+	char *cleaned_stash;
+	char *temp;
+	if (stash)
 	{
-		if (original_buffer[i] == '\n')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-char	*deload_buffer(char *original_buffer, int return_read, int fd)
-{
-	static char *stash;
-	if (return_read == 0)
-		return (NULL);
-	while (check_endoline(original_buffer) == 0 && return_read > 0)
-	{
-		stash = ft_strjoin(stash, original_buffer);
-		free(original_buffer);
-		return_read = read(fd, original_buffer, BUFFER_SIZE);
-	}
-	// if (return_read == 0)
-	// 	return (NULL);
-	if (check_endoline(stash) == 1)
-	{
+		// remettre au max BUFFER_SIZE au debut de stash
+		temp = ft_strchr(stash, '\n');
+		cleaned_stash = ft_stjoin()
+		
 		
 	}
-	
-	return (stash);
-}
+	else
+		return (NULL);
+}                                                                                                                                                                                                                                                                                                                                                                                
 
-char	*get_next_line(int fd)
+char	*get_next_line(int fd, char *current_line)
 {
-	int		return_read;
-	char	*original_buffer;
-	char *line;
+	// int		return_read;
+	char	*current_line;
+	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	original_buffer = (char*)ft_calloc(BUFFER_SIZE, 1);
-	if (!original_buffer)
-		return (NULL);
-	return_read = read(fd, original_buffer, BUFFER_SIZE);
-	if (return_read < 0)
-		return (NULL);
-	line = deload_buffer(original_buffer, return_read, fd);
-	return (line);
+	// ! Etape 1 : recupere ce qu'il reste de la stash
+	stash = reset_stash(fd, stash);
+	
+	return (current_line);
 }
