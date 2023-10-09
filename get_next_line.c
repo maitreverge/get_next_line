944a1731ec4ad1cd111db1b48036a67b4a17e7c5
flverge_42 <verge.f@gmail.com>
2023-10-09 12:08:52 +0200

    saving work

--- get_next_line.c
+++ get_next_line.c
@@ -3,50 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nope <nope@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:24:42 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/08 22:22:04 by nope             ###   ########.fr       */
+/*   Updated: 2023/10/09 12:08:38 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char 	*reset_stash(int fd, char *stash)
+// This fucntion will take a chunk until it meets a \n 
+char 	*big_chunk(int fd)
 {
-	char *cleaned_stash;
+	char *original_buffer;
 	char *temp;
-	if (stash)
-	{
-		// remettre au max BUFFER_SIZE au debut de stash
-		temp = ft_strchr(stash, '\n');
-		cleaned_stash = ft_stjoin()
-		
-		
-	}
-	else
+	int return_value_read;
+	// ! mallocing the real size
+	original_buffer = (char *)ft_calloc(BUFFER_SIZE + 1* sizeof(char)); // ? taille correcte
+	// original_buffer = (char *)ft_calloc(BUFFER_SIZE* sizeof(char));
+	if (!original_buffer)
 		return (NULL);
-}                                                                                                                                                                                                                                                                                                                                                                                
+	// ! Etape 1 : prendre du fd tant qu'il n'y a pas de \n
+	return_value_read = read(fd, original_buffer, BUFFER_SIZE);
+	while (return_value_read > 0 && ft_strchr(temp, '\n') == NULL)
+	{
+		if (return_value_read < 0)
+			return (NULL);
+		temp = ft_strjoin(temp, original_buffer);
+		return_value_read = read(fd, original_buffer, BUFFER_SIZE);
+	}
+	// Cas 1 : il n'y a rien dans la stash => simple copie
+	// Cas 2 : il y a deja des trucs, donc il faut join le bordel
+	free(original_buffer);
+	return (temp);
+}
 
-char	*get_next_line(int fd, char *current_line)
+char *extract_line(char *stash)
 {
-	// int		return_read;
+	
+}
+
+char	*get_next_line(int fd)
+{
 	char	*current_line;
 	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	// ! Etape 1 : recupere ce qu'il reste de la stash
-	stash = reset_stash(fd, stash);
+	stash = big_chunk(fd);
+	if (!stash)
+		return (NULL);
+	current_line = extract_line(stash);
+
+
+	
 	
 	return (current_line);
 }
 
 /*
-prends un gros bloc sa mere
+! Stategie 1
+prends un gros bloc sa grand-mere
 
 decouper le gros bloc en deux parties
 1 - Un buffer de la taille de la merde a retenir => line
 2 - le reste dans un static buffer et ouais mon gars
+
+! Strat 2 
+Garder deux buffers, avec une stash et un buffer init au size en fonction de la compilation
+1 => Deload le buff dans la stash, et je faire en bouclant la fonction read
+2 => Se garder un troisiemme buffer pour faire un ping-pong entre ce dernier et la stash
+? Comment malloc la bonne taille, sachant que ca peut varier de 1 a bcp sa mere
+
+
+! Strat 3 :
+Liste chainnees mdr non merci
+
+! Start 4 :
 
 */
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:25:13 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/08 10:15:20 by flverge          ###   ########.fr       */
+/*   Updated: 2023/10/09 10:53:21 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@
 
 // Function of gnl
 char	*get_next_line(int fd);
-char	*reading_fd(int fd, int return_read, char *original_buffer);
-int		check_endofile(char *original_buffer);
 
 
 // Functions of utils
@@ -29,9 +27,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
-
-
-
-
 
 #endif
