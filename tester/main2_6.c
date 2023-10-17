3eb614cfbedef418b9d01f4a7f1b5c36ea3517a8
flverge_42 <verge.f@gmail.com>
2023-10-17 12:33:50 +0200

    writting more test

---
+++ tester/main2_6.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main2_6.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 10:22:36 by flverge           #+#    #+#             */
+/*   Updated: 2023/10/17 12:26:05 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../get_next_line.h"
+#include <fcntl.h> // library for the O_RDONLY oepn's flg
+#include <stdio.h>
+#include <string.h>
+
+void displayTestSeparator(const char *text, int colorCode)
+{
+    int separatorLength = 38;  // Total length of the separator line
+    int textLength = strlen(text);
+    int paddingLength = separatorLength - textLength;
+    int leftPadding = paddingLength / 2;
+    int rightPadding = paddingLength - leftPadding;
+
+    printf("\033[%dm\n", colorCode);  // Set the desired text color
+    printf("  ///////////////////////////////////////////\n");
+    printf(" //%*s%s%*s //\n", leftPadding, "", text, rightPadding, "");
+    printf("///////////////////////////////////////////\n");
+    printf("\033[0m\n");  // Reset text color
+}
+
+void	bn(void)
+{
+	printf("\n");
+}
+
+void	heading(int i)
+{
+	if (i == 1)
+	{
+		printf("\033[32;1mEXPECTED_RESULT\033[0m\n");
+		return ;
+	}
+	printf("\033[91;1mGET_NEXT_LINE_RESULT\033[0m\n");
+}
+
+int main(void)
+{
+	displayTestSeparator("TEST WITHOUT BUFFER SIZE", 32);
+	bn();
+	heading(1);
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+
+	int fd;
+	char *master_buffer;
+	fd = open("file_tests/test_2.txt", O_RDONLY);
+	
+	heading(2);
+	while (1)
+	{
+		master_buffer = get_next_line(fd);
+		if (master_buffer == NULL)
+			break;
+		printf("%s", master_buffer);
+		free(master_buffer);
+	}
+	if (master_buffer)
+		free(master_buffer);
+	close (fd);
+}
--- tester/tester.sh
+++ tester/tester.sh
@@ -34,4 +34,45 @@ cc -Wall -Wextra -Werror -g main1_6.c ../get_next_line.c ../get_next_line_utils.
 
 sleep 1
 
+#################################################### test2
+
+rm test1
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=0 main2_1.c ../get_next_line.c ../get_next_line_utils.c -o test1 && valgrind ./test1
+
+sleep 1
+
+#################
+rm test2
+
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=-42 main2_2.c ../get_next_line.c ../get_next_line_utils.c -o test2 && valgrind ./test2
+
+sleep 1
+
+#################
+rm test3
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=1 main2_3.c ../get_next_line.c ../get_next_line_utils.c -o test3 && valgrind ./test3
+
+sleep 1
+
+#################
+rm test4
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=42 main2_4.c ../get_next_line.c ../get_next_line_utils.c -o test4 && valgrind ./test4
+
+sleep 1
+
+#################
+rm test5
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=100000 main2_5.c ../get_next_line.c ../get_next_line_utils.c -o test5 && valgrind ./test5
+
+sleep 1
+
+#################
+rm test6
+
+cc -Wall -Wextra -Werror -g main2_6.c ../get_next_line.c ../get_next_line_utils.c -o test6 && valgrind ./test6
+
+sleep 1
+
+
+
 
